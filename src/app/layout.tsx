e57a14d6621f341d01a212f3cabe7ce190c7261b
R3call <thenoobyzombie@gmail.com>
2025-02-09 14:48:29 +0100

    style: change name of project to mintlearn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <p className="text-lg flex items-center gap-x-2 font-extrabold px-20 py-4">
+          <Image
+            src="/mintlearn-logo.svg"
+            width={30}
+            height={30}
+            alt="MintLearn logo"
+          />
+          MintLearn
+        </p>
         {children}
+        <Toaster />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 import { QuizForm } from "@/components/quiz-form";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-lg grid px-4 py-12 items-center">
-      <h1 className="text-5xl flex gap-x-4 mb-8 font-extrabold">
-        <Image
-          src="/educhat-logo.svg"
-          width={50}
-          height={50}
-          alt="Educhat logo"
-        />
-        Educhat
-      </h1>
+      <h2 className="text-4xl mb-12 font-extrabold">Generate your quiz</h2>
       <QuizForm />
     </div>
   );
